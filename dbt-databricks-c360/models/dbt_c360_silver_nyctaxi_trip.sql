7053e32d4335e8d40bfbb7263a801fe7563d82bd
dustinvannoy-db <dustin.vannoy@databricks.com>
2023-06-08 14:57:38 -0700

    Set silver nyc taxi to use reference

--- dbt-databricks-c360/models/dbt_c360_silver_nyctaxi_trip.sql
+++ dbt-databricks-c360/models/dbt_c360_silver_nyctaxi_trip.sql
@@ -8,4 +8,4 @@ select
     ,regexp_replace(substring(tpep_pickup_datetime,1,7), '-', '_') as pickup_date
     ,to_date(tpep_pickup_datetime, "yyyy-MM-dd HH:mm:ss") as pickup_timestamp
     ,to_date(tpep_dropoff_datetime, "yyyy-MM-dd HH:mm:ss") as dropoff_timestamp
-from dbdemos.dbt_c360_tmp_vw_nyctaxi_trip
+from {{ref('dbt_c360_tmp_vw_nyctaxi_trip')}}
